43ae0799fb63bc6ff13bd11f8a89bcc2f60ddb59
Sara Eriksson <Sara@Saras-MacBook-Pro.local>
2017-02-27 20:06:32 +0100

    hej

--- js/controller/confirmDishController.js
+++ js/controller/confirmDishController.js
@@ -2,7 +2,7 @@
  * Created by dorapalfi on 27/02/17.
  */
 var confirmDishController = function(view, model){
-    view.confirmButton.click(function{
+    view.confirmButton.click(function(){
         
     })
 
